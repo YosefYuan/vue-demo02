087ce1a61bf44b60a931152243b1b40ef3e57cab
Yosef Yuan <yosyuan@publicisgroupe.net>
2017-08-04 18:35:58 +0800

    0804

---
+++ src/main.js
@@ -0,0 +1,56 @@
+import Vue from 'vue'
+import Layout from './components/layout'
+import VueRouter from 'vue-router'
+import IndexPage from './pages'
+import DetailPage from './pages/detail'
+import DetailAnalysis from './pages/detail/analysis'
+import DetailCount from './pages/detail/count'
+import DetailForecast from './pages/detail/forecast'
+import DetailPublish from './pages/detail/publish'
+import axios from 'axios'
+
+Vue.use(VueRouter)
+Vue.prototype.axios = axios
+
+let router = new VueRouter({
+  mode: 'history',
+  routes: [
+    {
+      path: '/',
+      component: IndexPage
+    },
+    {
+      path: '/detail',
+      component: DetailPage,
+      redirect: '/detail/count',
+      children: [
+        {
+          path: 'analysis',
+          component: DetailAnalysis
+        },
+        {
+          path: 'count',
+          component: DetailCount
+        },
+        {
+          path: 'forecast',
+          component: DetailForecast
+        },
+        {
+          path: 'publish',
+          component: DetailPublish
+        }
+      ]
+    }
+  ]
+})
+
+Vue.config.productionTip = false
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  router,
+  template: '<Layout/>',
+  components: { Layout }
+})
